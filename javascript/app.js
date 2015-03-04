c37cb76a9d2ea197d1177069a4b0f1478798338e
Lui <lui.pillmann@gmail.com>
2015-03-04 14:08:35 +0100

    Basic files added

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+(function () {
+	//var gem = {name: 'Dodecahedron', price: 2.95, description: '. . . '};
+	
+	var app = angular.module('myApp', []);//['ngResource']);
+
+	app.controller('MyController', function ($scope) {
+  		
+	});
+
+})();
