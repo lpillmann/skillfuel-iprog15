c5dff49b98a7a8b041b7aff6383b40b03c5809a6
Lui <lui.pillmann@gmail.com>
2015-03-06 16:56:20 +0100

    Try #1 with Firebase

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,16 @@
 (function () {
-	//var gem = {name: 'Dodecahedron', price: 2.95, description: '. . . '};
 	
-	var app = angular.module('myApp', []);//['ngResource']);
+	var app = angular.module('myApp', []);
+	var myFirebaseRef = new Firebase('https://sizzling-heat-4392.firebaseio.com/');
 
-	app.controller('MyController', function ($scope) {
-  		
+
+	app.controller('MyController', function () {
+  		this.data = '';
+
+  		myFirebaseRef.child("Person/Name").on("value", function(snapshot) {
+  			alert(snapshot.val());
+  			this.data = snapshot.val();
+		});
 	});
 
 })();
