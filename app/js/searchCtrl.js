c906bf127a032c14353e64193b08ff084f0370bd
Lui <lui.pillmann@gmail.com>
2015-03-11 16:47:37 +0100

    changes to include Modal+AngularJS

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,8 +1,18 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
-skillFuelApp.controller('SearchCtrl', function ($scope,SkillFuel) {
+skillFuelApp.controller('SearchCtrl', ['$scope', '$modal', '$log', 'SkillFuel', function ($scope,$modal,$log,SkillFuel) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
 
-});
+  $scope.getUsers = function () {
+    return SkillFuel.getUsers();
+  }
+
+  $scope.users = $scope.getUsers();
+
+
+
+
+
+}]);
