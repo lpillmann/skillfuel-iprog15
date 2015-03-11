5f3e5a354b7f75ad93e92556c111094ff90bd25b
jmahonen <mahonenjohanna@gmail.com>
2015-03-11 17:47:38 +0100

    Recovery after failed modal popup

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,6 +1,6 @@
 // Search controller that we use whenever we have a search inputs
 // and search results
-skillFuelApp.controller('SearchCtrl', ['$scope', '$modal', '$log', 'SkillFuel', function ($scope,$modal,$log,SkillFuel) {
+skillFuelApp.controller('SearchCtrl', ['$scope', 'SkillFuel', function ($scope,SkillFuel) {
 
   // TODO in Lab 5: you will need to implement a method that searchers for dishes
   // including the case while the search is still running.
