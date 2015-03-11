fae6ccec373817e68d4a091fcdf13fc0c5574071
Lui <lui.pillmann@gmail.com>
2015-03-11 20:54:51 +0100

    Deleted all files (all old) to include new ones

--- app/js/profileCtrl.js
+++
@@ -1,9 +0,0 @@
-// Dinner controller that we use whenever we want to display detailed
-// information for one dish
-skillFuelApp.controller('ProfileCtrl', function ($scope,$routeParams,SkillFuel) {
-  
-  // TODO in Lab 5: you need to get the dish according to the routing parameter
-  // $routingParams.paramName
-  // Check the app.js to figure out what is the paramName in this case
-  
-});
