90845e917003c68c95f41b61efea0612c9998a2d
Lui <lui.pillmann@gmail.com>
2015-03-15 18:21:51 +0100

    Better comments

--- app/js/databaseService.js
+++ app/js/databaseService.js
@@ -38,22 +38,28 @@ skillFuelApp.controller("SearchByTagsCtrl", ["$scope", "UserTags", "TagContent",
     // create a three-way binding to our Profile as $scope.profile
     $scope.userId = 'user1';
 
-    userTagsObj = UserTags($scope.userId);
+    userTagsObj = UserTags($scope.userId); //creates a UserTags Firebase Object that links to users.userId tags
     userTagsObj.$bindTo($scope, "tags");
 
-    userTagsObj.$loaded()
+    userTagsObj.$loaded() // executes the function after the object is properly loaded
         .then(function(data) {
-          $scope.needTags = [];
-          $scope.knowTags = [];          
+          $scope.needTags = []; // Arrays that will receive tag names got from query. Binded to scope, which means they can be called in the HTML
+          $scope.knowTags = []; // ng-repeat is used to iterate through them         
+          
           var i = 0; // indexes to debug 
           var j = 0;
 
-          angular.forEach(userTagsObj, function(value, key) { //key gets tagId, value gets true
-            tagObj = TagContent(key); // gets the tag content of the tags 
-            console.log("tagObj ref " + tagObj.$ref());
+          // Iterate through the  user tags object. For each tag, refers to its content in Firebase.tagsId, gets the tag name and puts it in 
+          // the proper array (needTags or knowTags).
+          angular.forEach(userTagsObj, function(value, key) { // 'key' gets tagId, 'value' gets true
+            tagObj = TagContent(key); // creates a TagContent Firebase Object that links to tags.tagId (tagId = key, comes from before) tags
+            console.log("tagObj CREATED for: " + tagObj.$ref()); 
+            
             tagObj.$loaded()
-              .then(function(data){
-                console.log("loaded tagObj " + data.$ref());
+              .then(function(data){ // executes the function after the object is properly loaded. 
+                // 'data' is equal to tagIds.tagX (one tag object listed in Firebase.tagsId) where tagX is 'key' from above 
+                console.log("tagObj LOADED for: " + data.$ref());
+                
                 if(data.isNeed === true) {
                   $scope.needTags.push(data.name);
                   console.log("needTags: " + $scope.needTags[i]);
@@ -64,16 +70,12 @@ skillFuelApp.controller("SearchByTagsCtrl", ["$scope", "UserTags", "TagContent",
                   console.log("knowTags: " + $scope.knowTags[j]);
                   j++;
                 }
+
               })
               .catch(function(error){
               console.error("Error:", error);
               });
           })
-          
-          console.log(data === userTagsObj); // true
-          console.log("entered firebase factory: " + userTagsObj.tag1);
-          console.log("entered firebase factory: " + userTagsObj.tag3);
-
         })
         .catch(function(error) {
           console.error("Error:", error);
