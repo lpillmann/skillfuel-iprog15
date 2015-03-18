ac77edd6fa1dd1e308e19fb7622d21c6e48e53a5
Lui <lui.pillmann@gmail.com>
2015-03-18 19:07:33 +0100

    Recovered profileCtrl

--- app/js/profileCtrl.js
+++ app/js/profileCtrl.js
@@ -1,12 +1,67 @@
-// Dinner controller that we use whenever we want to display detailed
-// information for one dish
-skillFuelApp.controller('ProfileCtrl', function ($scope,$routeParams,SkillFuel) {
-  
-  // TODO in Lab 5: you need to get the dish according to the routing parameter
-  // $routingParams.paramName
-  // Check the app.js to figure out what is the paramName in this case
-  
-});
+/** 
+  * @desc populates the profile view with user info from database (one of the conceptual system VIEWS --> populates page, among other controller-related functions)
+  * @required databaseServices.js 
+  * @used_by profile.html
+*/ 
+skillFuelApp.controller("ProfileCtrl", ["$scope", "ReadService",
+  function($scope, ReadService) {
+
+    $scope.userId = 'user1'; // TODO: make this parametrised according to navigation. Perhaps we should use $routeParams and app.js configurations
+
+    userBasicInfoLocal = ReadService.getUserBasicInfo($scope.userId);
+
+    // waits user basic info to load. When loaded, defines variable in $scope (visible to HTML)
+    userBasicInfoLocal.$loaded()
+      .then(function(data){ 
+        console.log("userBasicInfo LOADED for: " + data.$ref());
+        $scope.userBasicInfo = data;
+      })
+      .catch(function(error){
+      console.error("Error:", error);
+      });
+
+    userTagsObj = ReadService.UserTags($scope.userId); //creates a UserTags Firebase Object that links to users.userId tags
+    userTagsObj.$bindTo($scope, "tags");
+
+    userTagsObj.$loaded() // executes the function after the object is properly loaded
+        .then(function(data) {
+          $scope.needTags = []; // Arrays that will receive tag names got from query. Binded to scope, which means they can be called in the HTML
+          $scope.knowTags = []; // ng-repeat is used to iterate through them         
+          
+          var i = 0; // indexes to debug 
+          var j = 0;
+
+          // Iterate through the  user tags object. For each tag, refers to its content in Firebase.tagsId, gets the tag name and puts it in 
+          // the proper array (needTags or knowTags).
+          angular.forEach(userTagsObj, function(value, key) { // 'key' gets tagId, 'value' gets true
+            tagObj = ReadService.TagContent(key); // creates a TagContent Firebase Object that links to tags.tagId (tagId = key, comes from before) tags
+            console.log("tagObj CREATED for: " + tagObj.$ref()); 
+            
+            tagObj.$loaded()
+              .then(function(data){ // executes the function after the object is properly loaded. 
+                // 'data' is equal to tagIds.tagX (one tag object listed in Firebase.tagsId) where tagX is 'key' from above 
+                console.log("tagObj LOADED for: " + data.$ref());
+                
+                if(data.isNeed === true) {
+                  $scope.needTags.push(data.name);
+                  console.log("needTags: " + $scope.needTags[i]);
+                  i++;
+                }
+                else if (data.isNeed === false) {
+                  $scope.knowTags.push(data.name);
+                  console.log("knowTags: " + $scope.knowTags[j]);
+                  j++;
+                }
+              })
+              .catch(function(error){
+              console.error("Error:", error);
+              });
+          })
+        })
+        .catch(function(error) {
+          console.error("Error:", error);
+        }); 
+}]);
 
 
 skillFuelApp.directive('createUserProject', function(){
