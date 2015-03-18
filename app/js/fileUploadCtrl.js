503f1ad2522daa9b3e689a4c97ea985326a11d07
Johan Lindeberg <jlindebe@kth.se>
2015-03-18 22:42:18 +0100

    trying out imgur fileupload

--- app/js/fileUploadCtrl.js
+++ app/js/fileUploadCtrl.js
@@ -6,8 +6,25 @@ skillFuelApp.controller('FileUploadCtrl', ['$scope', '$upload', function ($scope
 	$scope.upload = function (files) {
 			if (files && files.length) {
 					for (var i = 0; i < files.length; i++) {
+							
+							var fileReader = new FileReader();
+							fileReader.readAsArrayBuffer(files[i]);
+							fileReader.onload = function(e) {
+									$upload.http({
+											url: 'https://api.imgur.com/3/image',
+											headers: {'Content-Type': file.type},
+											data: e.target.result
+									}).then(function(response) {
+											//success;
+									}, null, function(evt) {
+											$scope.progress[index] = parseInt(100.0 * evt.loaded / evt.total);
+									});
+							}
+							
+							
+							/*
 							var file = files[i];
-							$upload.upload({
+							$upload.http({
 									url: 'upload/url',
 									fields: {'username': $scope.username},
 									file: file
@@ -16,8 +33,13 @@ skillFuelApp.controller('FileUploadCtrl', ['$scope', '$upload', function ($scope
 									console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
 							}).success(function (data, status, headers, config) {
 									console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+							
+							
 							});
+							*/
 					}
 			}
 	};
 }]);
+
+    
