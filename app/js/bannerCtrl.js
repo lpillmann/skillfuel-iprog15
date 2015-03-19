7c8a3d977321d881ca1c7cf75080043d574fe26b
Johan Lindeberg <jlindebe@kth.se>
2015-03-20 00:33:43 +0100

    cleanup

--- app/js/bannerCtrl.js
+++ app/js/bannerCtrl.js
@@ -1,9 +1,5 @@
 skillFuelApp.controller('BannerCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
 	$scope.getMyId = function(){
-		//return "test";
 		return $rootScope.myid;
 	};
-	
-	$scope.myid = 3;
-
 }]);
