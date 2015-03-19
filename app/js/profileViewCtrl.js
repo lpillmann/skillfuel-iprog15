98b05962147790bbb960ec1f08cc5263c6b320b9
wencens <ronger.wencens@gmail.com>
2015-03-19 11:25:49 +0100

    Profile View changes

--- app/js/profileViewCtrl.js
+++ app/js/profileViewCtrl.js
@@ -70,22 +70,14 @@ skillFuelApp.directive('createUserProject', function(){
 		restrict: 'E',
 		templateUrl: 'partials/new-profile-projects.html', // current root is at from index.html
 		controller:function(){
-
 			this.addProject = false;
-
 			this.setAddProject = function(value) {
-
 				this.addProject = value;
-
 			};
-
 			this.isAddProject = function() {
-			
 				return this.addProject;
 			};
 		},
-
 		controllerAs: 'projectCtrl'
-
 	};
 });
