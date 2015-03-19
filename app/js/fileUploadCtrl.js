4d6b8dc31f3a6170dd3bf45169f3f608fb3e172e
Johan Lindeberg <jlindebe@kth.se>
2015-03-19 13:57:14 +0100

    small change in search-users for the needs/knows

--- app/js/fileUploadCtrl.js
+++ app/js/fileUploadCtrl.js
@@ -4,6 +4,8 @@ skillFuelApp.controller('FileUploadCtrl', ['$scope', '$upload', function ($scope
 	});
 
 	$scope.upload = function (files) {
+	
+	
 			if (files && files.length) {
 					for (var i = 0; i < files.length; i++) {
 							var file = files[i]
@@ -12,37 +14,18 @@ skillFuelApp.controller('FileUploadCtrl', ['$scope', '$upload', function ($scope
 							fileReader.onload = function(e) {
 									$upload.http({
 											url: 'https://api.imgur.com/3/image',
-											//headers: {'Content-Type': file.type},
 											headers: {
 												'Content-Type': file.type,
 												'Authorization': 'Client-ID 4858d2a8e5879a4'},
 											data: e.target.result
 									}).then(function(response) {
-											console.log('success, maybe? response: ' + JSON.stringify(response));//success;
+											console.log('success, maybe? response: ' + JSON.stringify(response) + 'LINK: ' + response.data.data.link);//success;
 									}, null, function(evt) {
 											$scope.progress = parseInt(100.0 * evt.loaded / evt.total);
 									});
 							}
 							
-							
-							/*
-							var file = files[i];
-							$upload.http({
-									url: 'upload/url',
-									fields: {'username': $scope.username},
-									file: file
-							}).progress(function (evt) {
-									var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-									console.log('progress: ' + progressPercentage + '% ' + evt.config.file.name);
-							}).success(function (data, status, headers, config) {
-									console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
-							
-							
-							});
-							*/
 					}
 			}
 	};
 }]);
-
-    
